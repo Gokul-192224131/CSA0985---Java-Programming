fd2a4b13aee92fe8ab1cd20dda4ea31c01e31bbd
Gokul-192224131 <123940275+Gokul-192224131@users.noreply.github.com>
2023-07-26 12:09:25 +0530

    Add files via upload

---
+++ AREA OF RECTANGLE USING CLASS.java
@@ -0,0 +1,23 @@
+PROGRAM:
+
+class Rect
+{
+            int l,b;
+}
+class CalAreaofRectangle
+{
+            public static void main (String args[])
+            {
+                        int a;
+                        Rect rect = new Rect();
+                        rect.l=20;
+                        rect.b=60;
+                        a=rect.l*rect.b;
+                        System.out.println("Area of Rectangle : "+a);
+            }
+}
+
+
+OUTPUT:
+
+Area of rectangle : 1200
